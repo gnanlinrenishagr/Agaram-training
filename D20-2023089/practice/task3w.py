0fcdc0f7e982e0ab5edddbecc3170bae49d709d7
gnanliinrenishagr <gnaninrenisha@gmail.com>
2023-08-11 13:51:43 +0530

    Karka traning

---
+++ D20-2023089/practice/task3w.py
@@ -0,0 +1,19 @@
+nums_list = [2, 5, 8, 1, 9, 3, 7]
+for num in range(len(nums_list)):
+    if nums_list[num]>nums_list[num+1]:
+        number=nums_list[num]-nums_list[num+1]
+    else:
+        number=nums_list[num+1]-nums_list[num]
+print(number)
+
+
+
+
+
+
+input= ["flower", "flow", "flight"]
+for each in range(len(input)):
+    for i in each:
+        if i[each]==i[each]:
+            ans=i[each]
+print(ans)
