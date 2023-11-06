5ee9d05bbb4d8cfab2a115acaa3aa338428c8562
gnanliinrenishagr <gnaninrenisha@gmail.com>
2023-11-06 09:26:18 +0530

    Agaram Training

---
+++ D34/React/my-app/src/Subheader.js
@@ -0,0 +1,8 @@
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// function Subheader(props){
+//     return(
+//         <span>from ,{props.newadd}</span>
+//     )
+// }
+// export default Subheader
